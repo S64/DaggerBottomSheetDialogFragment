7e6ad967872d78487472f75910019fe0dcbfd410
Shuma Yoshioka <s64.stdio@gmail.com>
2020-01-31 14:12:19 +0900

    Initial commit

---
+++ dagger-bottomsheet-dialog-fragment/src/main/java/jp/s64/android/dagger/bottomsheetdialogfragment/DaggerBottomSheetDialogFragment.java
@@ -0,0 +1,31 @@
+package jp.s64.android.dagger.bottomsheetdialogfragment;
+
+import android.content.Context;
+import android.support.design.widget.BottomSheetDialogFragment;
+
+import javax.inject.Inject;
+
+import dagger.android.AndroidInjector;
+import dagger.android.DispatchingAndroidInjector;
+import dagger.android.HasAndroidInjector;
+import dagger.android.support.AndroidSupportInjection;
+
+public abstract class DaggerBottomSheetDialogFragment
+        extends BottomSheetDialogFragment
+        implements HasAndroidInjector {
+
+    @Inject
+    DispatchingAndroidInjector<Object> androidInjector;
+
+    @Override
+    public void onAttach(Context context) {
+        AndroidSupportInjection.inject(this);
+        super.onAttach(context);
+    }
+
+    @Override
+    public AndroidInjector<Object> androidInjector() {
+        return androidInjector;
+    }
+
+}
